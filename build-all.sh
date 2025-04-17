12222c5c6a4c1255e076e6595ee1d773056203f7
Amos Wenger <amos@bearcove.eu>
2025-04-17 05:57:00 +0200

    Enable reflect, measure via hyperfine

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -eux
 
 for dir in facet_at_08 syn_at_2 syn_at_1; do
-    cargo clean --manifest-path "$dir/Cargo.toml"
-    cargo build --release --timings --manifest-path "$dir/Cargo.toml" "$@"
+    hyperfine --prepare "cargo clean --manifest-path $dir/Cargo.toml" "cargo build --release --timings --manifest-path $dir/Cargo.toml $@"
 done
