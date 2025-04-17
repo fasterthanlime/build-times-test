2fc0da3703cf430d93b843af18ef6fd23fcd4eec
Amos Wenger <amos@bearcove.eu>
2025-04-17 06:17:15 +0200

    better names

--- hyperfine.sh
+++ hyperfine.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env -S bash -eu
 
 dirs=(facet_at_08 syn_at_2 syn_at_1)
+names=("facet@0.8" "syn@2" "syn@1")
 cmds=()
-for dir in "${dirs[@]}"; do
-    cmds+=("cargo build --release --timings --manifest-path $dir/Cargo.toml $@")
+for i in "${!dirs[@]}"; do
+    dir="${dirs[$i]}"
+    name="${names[$i]}"
+    cmds+=("--command-name" "$name" "cargo build --release --timings --manifest-path $dir/Cargo.toml $@")
 done
-hyperfine --warmup 3 --prepare "rm -rf "**/target"" "${cmds[@]}"
+hyperfine --warmup 3 --max-runs 10 --prepare "rm -rf "**/target"" "${cmds[@]}"
